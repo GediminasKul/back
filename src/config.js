268ebcd24ff7131ec73c3989ef0a7d23cfb63042
Mantas M <mantasef@gmail.com>
2022-05-05 09:56:46 +0300

    hidden sensitive info dbCongif

--- src/config.js
+++ src/config.js
@@ -2,15 +2,6 @@ require('dotenv').config();
 
 const PORT = process.env.PORT || 5000;
 
-//test
-//test
-//test
-//test
-//test
-//test
-//test
-//test
-
 module.exports = {
   PORT,
 };
--- src/routes/dbsetup.js
+++ src/routes/dbsetup.js
@@ -1,10 +1,11 @@
-// require('dotenv').config();
+require('dotenv').config();
+
 const dbConfig = {
-  host: '194.135.87.110',
-  user: 'slscom_vetbee3',
-  password: 'aFP8WvZhSSXH2865',
-  database: 'slscom_vetbee3',
-  port: 3306,
+  host: process.env.host,
+  user: process.env.user,
+  password: process.env.password,
+  database: process.env.database,
+  port: process.env.port,
 };
 
 module.exports = {
